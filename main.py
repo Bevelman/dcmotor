dc9f52d74b578b5483a13775c8177c67a2c9378b
Bevelman <91498634+Bevelman@users.noreply.github.com>
2021-10-20 20:26:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+# Basic code for servo control using manual PWM
+# Set PWM period = 20 ms
+# 1 ms ON = full CW = 5% duty cycle
+# 2 ms ON = full CCW = 10% duty cycle
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+pwmPin = 16
+GPIO.setup(pwmPin, GPIO.OUT)
+# set min & max % duty cycles (5 and 10 are default values, but play
+# around to find optimum values for your motor)
+dcMin = 0
+dcMax = 100
+pwm = GPIO.PWM(pwmPin, 50) # PWM object at 50 Hz (20 ms period)
+pwm.start(0)
+try:
+  while True:
+    for dc in range(dcMin,dcMax):
+      pwm.ChangeDutyCycle(dcMax-dc)
+      print(dcMax-dc)
+      time.sleep(0.01)
+except KeyboardInterrupt:
+  print("bye")
+  GPIO.cleanup()
